9df4554c69b8a8cbadf92fe925c850bd2449be60
이유경 <a260@iyugyeong-ui-MacBookPro.local>
2024-09-07 16:43:44 +0900

    [init]: Seoullo 프로젝트 생성

---
+++ Seoullo/Seoullo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Seoullo
+//
+//  Created by 이유경 on 9/7/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Seoullo/Seoullo/SeoulloApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeoulloApp.swift
+//  Seoullo
+//
+//  Created by 이유경 on 9/7/24.
+//
+
+import SwiftUI
+
+@main
+struct SeoulloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
