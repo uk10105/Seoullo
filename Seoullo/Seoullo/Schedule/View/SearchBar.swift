cb66c9692ebee7ad1ff7ae071bdc7f1dd6266251
HyeS00 <hyesookim1118@gmail.com>
2024-09-11 21:51:04 +0900

    [design] searchBar 구현 #SEO-4

---
+++ Seoullo/Seoullo/Schedule/View/SearchBar.swift
@@ -0,0 +1,44 @@
+//
+//  SearchBar.swift
+//  Seoullo
+//
+//  Created by 김혜수 on 9/11/24.
+//
+
+import SwiftUI
+
+struct SearchBar: View {
+    @Binding var searchText: String
+    @State var isEditing = true
+    var handler: () -> Void
+    var body: some View {
+        HStack {
+            Image(systemName: "magnifyingglass")
+                .foregroundStyle(.point)
+            TextField("추가하고 싶은 장소를 입력해주세요.", text: $searchText)
+            if isEditing {
+                Button {
+                    isEditing = false
+                    searchText = ""
+                } label: {
+                    Text("취소")
+                }
+                .padding(.trailing, 25)
+                .transition(.move(edge: .trailing))
+            }
+        }
+        .frame(height: 50)
+        .frame(maxWidth: .infinity)
+        .padding(.horizontal, 25)
+        .background(Color.white)
+        .clipShape(.rect(cornerRadius: 15))
+        .onTapGesture {
+            isEditing = true
+        }
+
+    }
+}
+
+#Preview {
+    SearchBar(searchText: .constant("경복궁"), handler: {})
+}
