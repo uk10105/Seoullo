41a9c273d648f1703b84db4e3d9cc0b52acb5992
HyeS00 <hyesookim1118@gmail.com>
2024-09-15 19:50:44 +0900

    [feat] trip model 추가 #SEO-5

---
+++ Seoullo/Seoullo/Schedule/Model/Place.swift
@@ -0,0 +1,15 @@
+//
+//  Place.swift
+//  Seoullo
+//
+//  Created by 김혜수 on 9/15/24.
+//
+
+import Foundation
+
+struct Place {
+    var name: String
+    var x: Double
+    var y: Double
+    var memo: String?
+}
